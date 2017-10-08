0a446177234763b40a634be3d256bcc6aadc2ea8
walker <kirk.walker@cu.edu>
2017-10-07 22:47:05 -0600

    Commit 1.0.0 Added the logic for returning if the goal was reached and also changed the case of the path direction put into the path_to_goal.

--- driver.py
+++ driver.py
@@ -1,6 +1,17 @@
 from multiprocessing import Queue
 from Queue import Queue
-
+import time
+import test
+import sys
+if sys.platform == "win32":
+    import psutil
+    print("psutil", psutil.Process().memory_info().rss)
+# else:
+    # Note: if you execute Python from cygwin,
+    # the sys.platform is "cygwin"
+    # the grading system's sys.platform is "linux2"
+#     import resource
+#     print("resource", resource.getrusage(resource.RUSAGE_SELF).ru_maxrss)
 
 board_state = [1,2,5,3,4,0,6,7,8]
 parent = board_state
@@ -10,27 +21,52 @@ class CalcBoardSolution:
       
     use_search_algorithm_to = None
     tile_positions = None
+    start_time = None
       
     def with_the_search_algorithm(self,algorithm_string):
+        self.start_time = time.time()
         if (algorithm_string == "bfs"):
             self.use_search_algorithm_to = BFS()
         elif(algorithm_string == "dfs"):
             self.use_search_algorithm_to = DFS()
         return self
+    
     def and_the_board(self,tile_positions):
         self.tile_positions = tile_positions
         eight_puzzle_result = self.use_search_algorithm_to.get_the_optimal_solution_with_board(self.tile_positions)
+        eight_puzzle_result.running_time = time.time() - self.start_time
+        eight_puzzle_result.print_result()
         return eight_puzzle_result
     
       
 class EightPuzzleResult:
-    path_to_goal = []
+    path_to_solution = []
     cost_of_path = 0
     nodes_expanded = 0
     search_depth = 0
     max_search_depth = 0
     running_time = 0
     max_ram_usage = 0
+    
+    def get_path_to_solution(self,board_state):
+        if(board_state != None and board_state.parent_board != None):
+            self.path_to_solution.insert(0,board_state.direction)
+            self.cost_of_path += 1
+            self.search_depth += 1
+            self.get_path_to_solution(board_state.parent_board)
+        else:
+            self.max_search_depth += self.search_depth + 1
+            return self.path_to_solution
+        
+    def print_result(self):
+        print("path_to_goal:" + str(self.path_to_solution) )
+        print("cost_of_path:" + str(self.cost_of_path) )
+        print("nodes_expanded:" + str(self.nodes_expanded) )
+        print("search_depth:" + str(self.search_depth) )
+        print("max_search_depth:" + str(self.max_search_depth) )
+        print("running_time:" + str(self.running_time))
+        print("max_ram_usage:" + str(self.max_ram_usage))
+    
      
 class Frontier:
     """Simple example class"""
@@ -41,7 +77,6 @@ class Frontier:
     def left(self, parent_board_state):
         self.board_state = parent_board_state.copy_class()
         tile_position_array = self.board_state.current_board
-        self.direction = "left"
         self.swap_board_positions(tile_position_array, self.board_state.position_of_zero, self.board_state.position_of_zero - 1)
         self.board_state.position_of_zero = self.board_state.position_of_zero - 1
         return self.board_state
@@ -49,7 +84,6 @@ class Frontier:
     def right(self, parent_board_state):
         self.board_state = parent_board_state.copy_class()
         tile_position_array = self.board_state.current_board
-        self.direction = "right"
         self.swap_board_positions(tile_position_array, self.board_state.position_of_zero, self.board_state.position_of_zero + 1)
         self.board_state.position_of_zero = self.board_state.position_of_zero + 1
         return self.board_state
@@ -57,7 +91,6 @@ class Frontier:
     def up(self, parent_board_state):
         self.board_state = parent_board_state.copy_class()
         tile_position_array = self.board_state.current_board
-        self.direction = "up"
         self.swap_board_positions(tile_position_array, self.board_state.position_of_zero, self.board_state.position_of_zero - 3)
         self.board_state.position_of_zero = self.board_state.position_of_zero - 3
         return self.board_state
@@ -65,7 +98,6 @@ class Frontier:
     def down(self, parent_board_state):
         self.board_state = parent_board_state.copy_class()
         tile_position_array = self.board_state.current_board
-        self.direction = "down"
         self.swap_board_positions(tile_position_array, self.board_state.position_of_zero, self.board_state.position_of_zero + 3)
         self.board_state.position_of_zero += 3
         return self.board_state
@@ -80,6 +112,7 @@ class BoardState:
     position_of_zero = 0
     direction = None
     current_board = None
+    parent_board = None
     
     def copy_class(self):
         board_state = BoardState()
@@ -119,56 +152,61 @@ class BFS(SearchAlgorithm):
     def expand_node(self, direction, eight_puzzle_result, bfs_queue, new_board_state, current_board_state):
         if (self.solution_is_not_found): 
             new_board_state.direction = direction
+            new_board_state.parent_board = current_board_state
             if(self.the_union_of_the_frontier_and_explored_nodes().does_not_contain(new_board_state.current_board)):
                 self.frontier_queue.put_nowait(new_board_state)
-            eight_puzzle_result.nodes_expanded += 1
+                eight_puzzle_result.nodes_expanded += 1
+            
             if (new_board_state.current_board == self.optimal_solution):
                 self.solution_is_not_found = False
     
-        return current_board_state
+        return new_board_state
 
+    def frontier_queue_is_not_empty(self):
+        return not self.frontier_queue.empty()
+    
     def get_the_optimal_solution_with_board(self,tile_number_array):
-        eight_puzzle_result = EightPuzzleResult
-#         eight_puzzle_result.path_to_goal = ['Up','Left','Left']
-#         eight_puzzle_result.cost_of_path = 3
-#         eight_puzzle_result.nodes_expanded = 10
-#         eight_puzzle_result.search_depth = 3
-#         eight_puzzle_result.max_search_depth = 4
+        eight_puzzle_result = EightPuzzleResult()
         frontier = Frontier()
         self.frontier_queue = Queue()
         self.explored_set = set()
         board_state = BoardState()
         board_state.current_board = tile_number_array;
         board_state.position_of_zero = self.get_location_of_zero(tile_number_array)
-        self.frontier_queue.put_nowait(board_state)
         self.explored_set.add(board_state)
-        while(self.solution_is_not_found):
+        while(board_state.current_board != self.optimal_solution):
+            
             if (0 < board_state.position_of_zero < 9):
-                eight_puzzle_result.cost_of_path += 1
-                eight_puzzle_result.search_depth += 1
                 if(5 < board_state.position_of_zero < 9):
-                    self.expand_node("up",eight_puzzle_result, self.frontier_queue, frontier.up(board_state), board_state)
+                    self.expand_node("Up",eight_puzzle_result, self.frontier_queue, frontier.up(board_state), board_state)
                 elif(2 < board_state.position_of_zero < 6):
-                    self.expand_node("up",eight_puzzle_result, self.frontier_queue, frontier.up(board_state), board_state)
-                    self.expand_node("down",eight_puzzle_result, self.frontier_queue, frontier.down(board_state), board_state)
+                    self.expand_node("Up",eight_puzzle_result, self.frontier_queue, frontier.up(board_state), board_state)
+                    self.expand_node("Down",eight_puzzle_result, self.frontier_queue, frontier.down(board_state), board_state)
                 elif(-1 < board_state.position_of_zero < 3):
-                    self.expand_node("down",eight_puzzle_result, self.frontier_queue, frontier.down(board_state), board_state)
+                    self.expand_node("Down",eight_puzzle_result, self.frontier_queue, frontier.down(board_state), board_state)
                                     
                 if((board_state.position_of_zero + 1) % 3 == 0):
-                    self.expand_node("left",eight_puzzle_result, self.frontier_queue, frontier.left(board_state), board_state)
+                    self.expand_node("Left",eight_puzzle_result, self.frontier_queue, frontier.left(board_state), board_state)
                 elif((board_state.position_of_zero + 2) % 3 == 0):
-                    self.expand_node("left",eight_puzzle_result, self.frontier_queue, frontier.left(board_state), board_state)
-                    self.expand_node("right",eight_puzzle_result, self.frontier_queue, frontier.right(board_state), board_state)
+                    self.expand_node("Left",eight_puzzle_result, self.frontier_queue, frontier.left(board_state), board_state)
+                    self.expand_node("Right",eight_puzzle_result, self.frontier_queue, frontier.right(board_state), board_state)
                 elif((board_state.position_of_zero + 3) % 3 == 0):
-                    self.expand_node("right",eight_puzzle_result, self.frontier_queue, frontier.right(board_state), board_state)
+                    self.expand_node("Right",eight_puzzle_result, self.frontier_queue, frontier.right(board_state), board_state)
             else:
                 print "Error: Empty space out of the bounds of the board_state"
                 return None
-            print self.frontier_queue.qsize()
-            print board_state.current_board
+#             print self.frontier_queue.qsize()
+#             print board_state.current_board
+
+#             resource.test_getrusage()
+#             mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+            if(self.frontier_queue.empty()):
+                return None
+            
             board_state = self.frontier_queue.get_nowait()
             self.explored_set.add(board_state)
-            eight_puzzle_result.path_to_goal.append(board_state.direction)
+        
+        eight_puzzle_result.get_path_to_solution(board_state)
         return eight_puzzle_result
 
 class DFS(SearchAlgorithm):
